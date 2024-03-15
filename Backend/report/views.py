d92fedf5ab35f419124c6282f1e36d8ddb339051
Louange <benithaiyuyisenga2002@gmail.com>
2024-03-15 10:25:45 +0200

    changes

--- Backend/report/views.py
+++ Backend/report/views.py
@@ -18,15 +18,6 @@ class GenerateReportAPIView(APIView):
             if not start_date or not end_date:
                 return Response({"error": "start_date and end_date are required."}, status=400)
 
-            # Convert start_date and end_date to datetime objects
-            # start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
-            # end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
-
-            # Retrieve StockIn objects between start_date and end_date
-            # queryset = StockIn.objects.filter(
-            #     Q(created_at=start_date) &
-            #     Q(created_at=end_date)
-            # ).order_by("-id")
             queryset = StockIn.objects.filter(
                 delivered_date__gte=start_date,
                 delivered_date__lte=end_date
